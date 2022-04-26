e6139f60660b96d8d8a163c07e305e904a12181b
foxxxesky <abdurrahman1467@gmail.com>
2022-04-27 06:49:18 +0800

    bottom navbar

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -16,10 +16,59 @@ class MainPage extends StatelessWidget {
       );
     }
 
+    // Bottom Navbar
+    Widget customBottomNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/home_icon.png',
+                  width: 21,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/chat_icon.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/favorites_icon.png',
+                  width: 20,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/profile_nav.png',
+                  width: 18,
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomNav(),
       body: Center(
         child: Text('Main Page'),
       ),
