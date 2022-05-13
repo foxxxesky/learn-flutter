8bc17f2ea11d37920884045e32f7428ba6f933a8
foxxxesky <abdurrahman1467@gmail.com>
2022-05-13 20:57:44 +0800

    home page header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,11 +4,54 @@ import 'package:shamo_apps/theme.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello, Syariif',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semibold,
+                    ),
+                  ),
+                  Text(
+                    '@foxxxesky',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/profile_image.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
